983095bb17f020307c3399736e23991f0430b799
joshparkerj <superfakek@gmail.com>
2018-10-20 10:15:49 -0600

    complete basic set up

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const massive = require('massive');
+const dotenv = require('dotenv').config();
+
+
 const app = express();
+app.use(bodyParser.json());
+
+massive(process.env.DBURI)
+  .then(db => {
+    app.set('db',db);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 app.get('/health', (req,res) => {
   res.send('ok');
